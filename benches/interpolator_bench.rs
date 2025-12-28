5208c4656b470c4909b1c329a8c33e8c9e658ba5
ph1losof <my.tentacles.are.in.you@gmail.com>
2025-12-28 15:45:48 +0100

    fix: warning related to unused config in interpolator_bench

--- benches/interpolator_bench.rs
+++ benches/interpolator_bench.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use germi::{Germi, Config};
+use germi::Germi;
 
 fn benchmark_interpolator(c: &mut Criterion) {
     let mut group = c.benchmark_group("interpolator");
